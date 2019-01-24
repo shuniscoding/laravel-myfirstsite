5a96b99130adac737c519ca71aa5aeda5efe16c4
shun <shunichiroid@gmail.com>
2019-01-25 08:52:49 +0900

    Add page controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+
+        return view('welcome',[
+            'foo' => 'bar',
+        ]);
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 @extends('layout')
 
 @section('content')
-    <h1>My first Website</h1>
+    <h1>My first {{ $foo }} Website</h1>
+
+    {{-- <ul>
+        @foreach ($tasks as $task)
+            <li>{{ $task }}</li>
+        @endforeach
+    </ul> --}}
 @endsection
